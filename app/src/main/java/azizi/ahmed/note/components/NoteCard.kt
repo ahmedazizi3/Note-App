57932deff04fb7e6cfbc652f39187430888dff1f
DELL <aziziahmed027@gmail.com>
2024-11-26 11:41:13 +0100

    Not final commit, still there is room database

--- app/src/main/java/azizi/ahmed/note/components/NoteCard.kt
+++ app/src/main/java/azizi/ahmed/note/components/NoteCard.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import azizi.ahmed.note.model.Note
--- app/src/main/java/azizi/ahmed/note/screens/NoteScreen.kt
+++ app/src/main/java/azizi/ahmed/note/screens/NoteScreen.kt
@@ -1,7 +1,6 @@
 package azizi.ahmed.note.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
